09220e5a0e26f0828ade723fe313cb4bd7f4d8b4
CynGames <tomasm.leguizamon@gmail.com>
2022-11-22 14:02:20 -0300

    cors

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const logger = require('morgan');
 const dotenv = require("dotenv");
 
 const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 dotenv.config()
 
+app.use(cors({origin: 'https://csr-y8dm.onrender.com/'}))
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use('/', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use("/", indexRouter);
 
